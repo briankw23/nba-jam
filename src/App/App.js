ddb33d63d3133606ec42b51aac8a19f521d4a34d
Brian Wright <briankw23@gmail.com>
2018-07-19 11:56:21 -0500

    setup firebase connection

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,17 +10,61 @@ import MyTeam from '../components/MyTeam/MyTeam';
 import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 
+import fbConnection from '../firebaseRequests/connection';
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: '/orders', state: { from: props.location } }}
+            />
+          )
+      }
+    />
+  );
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: '/login', state: { from: props.location } }}
+            />
+          )
+      }
+    />
+  );
+};
+
 class App extends Component {
+
+  state = {
+    authed: false,
+  }
+
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <div>
-          <NavBar/>
+            <NavBar />
             <div className="container">
               <div className="row">
                 <Switch>
-                  <Route path="/" exact component= {Home}/>
+                  <Route path="/" exact component={Home} />
+                  <PublicRoute path="/register" authed={this.state.authed} component={Register} />
+                  <PrivateRoute path="/myTeam" authed={this.state.authed} component={MyTeam} />
                 </Switch>
               </div>
             </div>
