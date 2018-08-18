118364777164533673d109f01ed5f3609c23c8ef
Brian Wright <briankw23@gmail.com>
2018-08-18 00:38:57 -0500

    player images are showing

--- src/components/Verses/Verses.js
+++ src/components/Verses/Verses.js
@@ -23,8 +23,32 @@ class Verses extends  React.Component {
       return team.image;
     });
 
-    console.error('verse', playerOneTeamLogo[0]);
+    const pOneStarterOneImage = this.props.location.state.playerOneRoster.filter((player) => {
+      return player.id === this.props.location.state.pOneStarterOne;
+    }).map((player, index, array) => {
+      return player.image;
+    });
 
+    const pOneStarterTwoImage = this.props.location.state.playerOneRoster.filter((player) => {
+      return player.id === this.props.location.state.pOneStarterTwo;
+    }).map((player, index, array) => {
+      return player.image;
+    });
+
+    const pTwoStarterOneImage = this.props.location.state.playerTwoRoster.filter((player) => {
+      return player.id === this.props.location.state.pTwoStarterOne;
+    }).map((player, index, array) => {
+      return player.image;
+    });
+
+    const pTwoStarterTwoImage = this.props.location.state.playerTwoRoster.filter((player) => {
+      return player.id === this.props.location.state.pTwoStarterTwo;
+    }).map((player, index, array) => {
+      return player.image;
+    });
+
+    console.error('verse', playerOneTeamLogo[0]);
+    console.error('verse', pOneStarterOneImage[0]);
     const playerTwoTeamLogo = this.props.location.state.teams.filter((team) => {
       return team.id === this.props.location.state.playerTwoTeamImage;
     }).map((team, index, array) => {
@@ -43,8 +67,8 @@ class Verses extends  React.Component {
             <h1 className="finalText">FINAL GAME STATS:</h1>
           </div>
           <div className="col-lg-3 score">
-            <img className="col-lg-6 teamLogos" src={playerTwoTeamLogo[0]} alt=""/>
             <h1 className="col-lg-6 scoreText">78</h1>
+            <img className="col-lg-6 teamLogos" src={playerTwoTeamLogo[0]} alt=""/>
           </div>
         </div>
         {/* Second Section */}
@@ -53,6 +77,7 @@ class Verses extends  React.Component {
           <div className="col-lg-6">
             <div className="col-lg-4">
               {/* player one s one image */}
+              <img src={pOneStarterOneImage[0]} alt=""/>
             </div>
             <div className="col-lg-8">
 
@@ -62,6 +87,7 @@ class Verses extends  React.Component {
           <div className="col-lg-6">
             <div className="col-lg-4">
               {/* player two s one image */}
+              <img src={pTwoStarterOneImage[0]} alt=""/>
             </div>
             <div className="col-lg-8">
 
@@ -71,6 +97,7 @@ class Verses extends  React.Component {
           <div className="col-lg-6">
             <div className="col-lg-4">
               {/* player one s2 image */}
+              <img src={pOneStarterTwoImage[0]} alt=""/>
             </div>
             <div className="col-lg-8">
 
@@ -80,6 +107,7 @@ class Verses extends  React.Component {
           <div className="col-lg-6">
             <div className="col-lg-4">
               {/* player two s two image */}
+              <img src={pTwoStarterTwoImage[0]} alt=""/>
             </div>
             <div className="col-lg-8">
 
